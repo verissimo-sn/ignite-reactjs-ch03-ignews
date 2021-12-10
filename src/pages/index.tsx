8f10a543ff281988282fda7871345b8cc04fec31
verissimo-sn <verissimo_sn@hotmail.com>
2021-12-10 07:44:17 -0300

    feat: import global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
-    <h1 className={styles.title}>Start a project</h1>
+    <h1>Start a project</h1>
   )
 }
