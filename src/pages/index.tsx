0692f82c9a5b1ccfb6a2316c6993185bf90ece78
verissimo-sn <verissimo_sn@hotmail.com>
2021-12-16 06:53:06 -0300

    feat: add subscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { SubscribeButton } from '../components/SubscribeButton';
 import style from './home.module.scss';
 
 export default function Home() {
@@ -10,6 +11,8 @@ export default function Home() {
           Get access to all the publications <br/>
           <span>for $9.90 month</span>
         </p>
+
+        <SubscribeButton />
       </section>
 
       <img src="/images/avatar.svg" alt="Girl coding" />
