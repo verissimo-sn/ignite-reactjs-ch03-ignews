9399faad20e0569e67fd6000bd10e438eb34c99a
verissimo-sn <verissimo_sn@hotmail.com>
2021-12-16 06:52:27 -0300

    feat: create SubscribeButton component

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import style from './style.module.scss';
+
+export const SubscribeButton = () => {
+  return (
+    <button className={style.subscribeButton}>
+      Subscri now
+    </button>
+  );
+} 
