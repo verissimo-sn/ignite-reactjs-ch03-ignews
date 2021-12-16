54f9ae4f86d2e9c327a127e21e24fff69fd69fb9
verissimo-sn <verissimo_sn@hotmail.com>
2021-12-16 07:38:13 -0300

    feat: call stripe and get price through the serverSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { GetServerSideProps } from 'next';
+
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 import style from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <main className={style.contentContainer}>
       <section className={style.hero}>
@@ -9,13 +19,31 @@ export default function Home() {
         <h1>New about the <span>React</span> world.</h1>
         <p>
           Get access to all the publications <br/>
-          <span>for $9.90 month</span>
+          <span>for {product.amount} month</span>
         </p>
 
-        <SubscribeButton />
+        <SubscribeButton priceId={product.priceId} />
       </section>
 
       <img src="/images/avatar.svg" alt="Girl coding" />
     </main>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { id, unit_amount } = await stripe.prices.retrieve('price_1K7Gl5IVUW9yporc7nlporbn');
+  
+  const product = {
+    priceId: id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(unit_amount/100)
+  }
+
+  return { 
+    props: {
+      product
+    }
+  }
 }
