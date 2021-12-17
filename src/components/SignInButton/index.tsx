0e9673ce8d515ca9ad1115b3dcb2509cf0644c80
verissimo-sn <verissimo_sn@hotmail.com>
2021-12-17 07:44:09 -0300

    feat: social login

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,29 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
 
 export const SignInButton = () => {
-  const isUserLoggedIn = true;
+  const { data , status } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return (status === 'authenticated') ? (
+    <button 
+      type="button" 
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04D361" />
-      Verissimo Santos
+      {data.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button 
+      type="button" 
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#EBA417" />
       Sign in with Github
     </button>
