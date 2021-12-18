45c64581483c0f039fa55704bad5b4c61940ab9e
verissimo-sn <verissimo_sn@hotmail.com>
2021-12-18 09:03:48 -0300

    feat: save faunaDb user data

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -8,4 +11,25 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user: { email }, account, profile }) {
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {
+              data: {
+                email
+              }
+            }
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+
+    }
+  }
 });
