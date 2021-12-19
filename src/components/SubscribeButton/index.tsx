5c2b75a0cbeb4469f202a09f67ae97ff4304c26f
verissimo-sn <verissimo_sn@hotmail.com>
2021-12-19 09:09:14 -0300

    feat: add subscribe method

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/react';
 import style from './style.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,8 +6,22 @@ interface SubscribeButtonProps {
 }
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const { status } = useSession(); 
+
+  const handleSubscribe = () => {
+    if(status !== 'authenticated') {
+      signIn('github');
+      return;
+    }
+
+    
+  }
+
   return (
-    <button className={style.subscribeButton}>
+    <button
+      className={style.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscri now
     </button>
   );
