50b1a839c13699a8e63e139f613e6a99bb8a7234
verissimo-sn <verissimo_sn@hotmail.com>
2021-12-19 08:35:24 -0300

    feat: create user if not exists

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -15,14 +15,26 @@ export default NextAuth({
     async signIn({ user: { email }, account, profile }) {
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            {
-              data: {
-                email
-              }
-            }
-          )
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {  data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              ),
+            ),
+          ),
         );
 
         return true;
